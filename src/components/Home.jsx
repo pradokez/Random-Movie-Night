4b2b669b448b868b92d3ff0949dcf6ea96349535
pradokez <pradokeziah@gmail.com>
2024-02-15 21:11:50 -0300

    wip

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 const Home = ({handleClick}) => {
     return (
-    <div>
-      <h1>random movie night</h1>
+    <div className="wrapper">
+      <h1 className="title"><span className="first-line">random</span><span className="second-line">movie night</span></h1>
       <button onClick={handleClick}>start</button>
     </div>
     )
