e0f172d7e3a1e7d775ecc295b60dfb4e356fc2b7
pradokez <pradokeziah@gmail.com>
2024-02-20 20:42:24 -0300

    style: wip ellipsis

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ const Home = ({handleClick}) => {
     <div className="wrapper">
       <h1 className="title"><span className="first-line">random</span><span className="second-line">movie night</span></h1>
       <button onClick={handleClick}>start</button>
+      <div className="ellipse"/>
     </div>
     )
 }
