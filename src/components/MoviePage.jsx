307363190dba7ec5aa063e4cd4ed56b6b8e749bc
pradokez <pradokeziah@gmail.com>
2024-02-20 20:14:38 -0300

    wip

--- src/components/MoviePage.jsx
+++ src/components/MoviePage.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
 const MoviePage = ({movie}) => {
+    console.log(movie)
     const banner = movie.posterURLs.original;
     return (
-        <>
+        <div className="movie-info">
             <img src={banner} width={342}/>
-            <h1>{movie.originalTitle}</h1>
+            <h1>{`${movie.originalTitle} (${movie.year})`}</h1>
             <p>{movie.overview}</p>
-        </>
+        </div>
     )
 }
 
