9bd69df5d3ca35683aa17e20182b188dfc52429c
pradokez <pradokeziah@gmail.com>
2024-03-11 18:37:54 -0300

    style: desktop

--- src/components/MoviePage.jsx
+++ src/components/MoviePage.jsx
@@ -3,11 +3,15 @@ const MoviePage = ({movie}) => {
     console.log(movie)
     const banner = movie.posterURLs.original;
     return (
+        <>
         <div className="movie-info">
             <img src={banner} width={342}/>
             <h1>{`${movie.originalTitle} (${movie.year})`}</h1>
             <p>{movie.overview}</p>
         </div>
+        <div className="semi-ellipse-top"/>
+        <div className="semi-ellipse-bottom"/>
+        </>
     )
 }
 
