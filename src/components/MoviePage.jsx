6cfcbd5222196c7d2bbbc005d36a28f94e0a7f9d
pradokez <pradokeziah@gmail.com>
2024-04-08 20:08:14 -0300

    feat: add movie url to streaming icon

--- src/components/MoviePage.jsx
+++ src/components/MoviePage.jsx
@@ -1,15 +1,15 @@
 /* eslint-disable react/prop-types */
 const MoviePage = ({movie}) => {
-    console.log(movie)
     const banner = movie.posterURLs[342];
     const streamingInfoArray = Object.entries(movie.streamingInfo);
     const streaming = streamingInfoArray[0][0];
+    const movieUrl = streamingInfoArray[0][1].us.link;
 
     return (
         <>
         <div className="movie-info">
             <div className='movie-banner' style={{backgroundImage: `url(${banner})`,}}>
-            <a href=""><img src={`/src/assets/icon-${streaming}.svg`} style={{margin: '10px', zIndex: 3}}/></a>
+            <a href={movieUrl} target="_blank" rel="noopener noreferrer"><img src={`/src/assets/icon-${streaming}.svg`} style={{margin: '10px', zIndex: 3}}/></a>
             </div>
             <h1>{`${movie.originalTitle} (${movie.year})`}</h1>
             <p>{movie.overview}</p>
