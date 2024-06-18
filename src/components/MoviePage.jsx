e1b805fca640f8c9dbd9b4bf7a34af6e3506dc72
pradokez <pradokeziah@gmail.com>
2024-06-17 21:28:28 -0300

    style: mobile and responsive

--- src/components/MoviePage.jsx
+++ src/components/MoviePage.jsx
@@ -14,7 +14,7 @@ const MoviePage = ({movie}) => {
             <div className='movie-banner' style={{backgroundImage: `url(${banner})`,}}>
             <a href={movieUrl} target="_blank" rel="noopener noreferrer"><img src={`/src/assets/icon-${streaming}.svg`} style={{margin: '10px', zIndex: 3}}/></a>
             </div>
-            <h1>{`${originalTitle} (${year})`}</h1>
+            <h1><a href={movieUrl} target="_blank" rel="noopener noreferrer">{`${originalTitle} (${year})`}</a></h1>
             <p>{overview}</p>
         </div>
         <div className="semi-ellipse-top"/>
