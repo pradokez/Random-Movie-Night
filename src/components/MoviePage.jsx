aca7efd6280b3baf21440b60401a06262f66990c
pradokez <pradokeziah@gmail.com>
2024-06-17 21:34:02 -0300

    refactor: move assets

--- src/components/MoviePage.jsx
+++ src/components/MoviePage.jsx
@@ -12,7 +12,7 @@ const MoviePage = ({movie}) => {
         <Fragment>
         <div className="movie-info">
             <div className='movie-banner' style={{backgroundImage: `url(${banner})`,}}>
-            <a href={movieUrl} target="_blank" rel="noopener noreferrer"><img src={`/src/assets/icon-${streaming}.svg`} style={{margin: '10px', zIndex: 3}}/></a>
+            <a href={movieUrl} target="_blank" rel="noopener noreferrer"><img className="streaming-logo" src={`/public/icon-${streaming}.svg`}/></a>
             </div>
             <h1><a href={movieUrl} target="_blank" rel="noopener noreferrer">{`${originalTitle} (${year})`}</a></h1>
             <p>{overview}</p>
